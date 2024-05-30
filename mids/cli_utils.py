c77adb4f2d965f2efd9ff3aa5222cfb96ec77e49
Josh Humphries <j.humphries@nhm.ac.uk>
2024-05-30 18:35:24 +0100

    chore: add helper functions for retrieving data from URLs
    This means we can get rid of httpx dependency and provide a bit more sanity in how we load the data & check for errors

--- mids/cli_utils.py
+++ mids/cli_utils.py
@@ -1,3 +1,7 @@
+import json
+import urllib.request
+from urllib.error import HTTPError
+
 from mids.lib import init
 from mids.model import Discipline
 
@@ -35,3 +39,30 @@ def print_check(data: dict):
     """
     level = init(Discipline.biology).check(data)
     print(f"Matched to MIDS level {level}")
+
+
+def get_gbif_data(gbif_id: int) -> dict | None:
+    """
+    Retrieves the data for the given GBIF ID from the GBIF API.
+
+    :param gbif_id: the GBIF ID of the occurrence
+    :return: either data as a dict or None if the occurrence does not exist
+    """
+    return get_data_from_url(f"https://api.gbif.org/v1/occurrence/{gbif_id}")
+
+
+def get_data_from_url(url: str) -> dict | None:
+    """
+    Retrieves JSON data from the given URL and returns it, or None if the request fails.
+
+    :param url: the URL to retrieve data from
+    :return: either data as a dict or None if a response cannot be retrieved
+    """
+    try:
+        with urllib.request.urlopen(url) as r:
+            if r.headers["Content-Type"] != "application/json":
+                return None
+            data = r.read().decode("utf-8")
+            return json.loads(data)
+    except HTTPError:
+        return None
