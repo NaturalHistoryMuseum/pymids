98511747f2d99e5cea2ba74881870e9bdc6fc6d9
Josh Humphries <j.humphries@nhm.ac.uk>
2024-05-30 18:36:13 +0100

    refactor: rearrange the cli module to use new url get functions and remove incorrect verbose flag usage

--- mids/cli.py
+++ mids/cli.py
@@ -2,10 +2,8 @@ import json
 from pathlib import Path
 
 import click
-import httpx
-import requests
 
-from mids.cli_utils import print_check, print_report
+from mids.cli_utils import print_check, print_report, get_gbif_data, get_data_from_url
 
 
 @click.group("mids")
@@ -17,8 +15,8 @@ def cli():
 @click.argument("url", type=click.STRING)
 @click.option("-v", "--verbose", is_flag=True, default=False)
 def report_url(url: str, verbose: bool = False):
-    with requests.get(url) as r:
-        print_report(r.json(), verbose=verbose)
+    data = get_data_from_url(url)
+    print_report(data, verbose=verbose)
 
 
 @cli.command("report-file")
@@ -33,33 +31,35 @@ def report_file(path: Path, verbose: bool = False):
 @click.argument("gbif_id", type=click.INT)
 @click.option("-v", "--verbose", is_flag=True, default=False)
 def report_gbif(gbif_id: int, verbose: bool = False):
-    gbif_url = f"https://api.gbif.org/v1/occurrence/{gbif_id}"
-    response = httpx.get(gbif_url)
-    print_report(response.json(), verbose=verbose)
+    data = get_gbif_data(gbif_id)
+    print_report(data, verbose=verbose)
 
 
 @cli.command("check-url")
 @click.argument("url", type=click.STRING)
 def check_url(url: str):
-    with requests.get(url) as r:
-        print_check(r.json())
+    data = get_data_from_url(url)
+    if data is None:
+        print(f"No JSON data could be loaded from the URL {url}")
+    else:
+        print_check(data)
 
 
 @cli.command("check-file")
 @click.argument("file", type=click.File())
-@click.option("-v", "--verbose", is_flag=True, default=False)
-def check_file(path: Path, verbose: bool = False):
+def check_file(path: Path):
     with path.open() as f:
         print_check(json.load(f))
 
 
 @cli.command("check-gbif")
 @click.argument("gbif_id", type=click.INT)
-@click.option("-v", "--verbose", is_flag=True, default=False)
-def check_gbif(gbif_id: int, verbose: bool = False):
-    gbif_url = f"https://api.gbif.org/v1/occurrence/{gbif_id}"
-    response = httpx.get(gbif_url)
-    print_check(response.json())
+def check_gbif(gbif_id: int):
+    gbif_data = get_gbif_data(gbif_id)
+    if gbif_data is None:
+        print(f"No occurrence with ID {gbif_id} found")
+    else:
+        print_check(gbif_data)
 
 
 if __name__ == "__main__":
